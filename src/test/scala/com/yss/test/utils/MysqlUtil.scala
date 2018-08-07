0e47fd11022c621fd6306a30f8241ced7b336437
zhangyunlin@ysstech.com <zhangyunlin@ysstech.com>
2018-08-07 16:48:36 +0800

    imcexchangerate.xml文件读取和写入

---
+++ src/test/scala/com/yss/test/utils/MysqlUtil.scala
@@ -0,0 +1,24 @@
+package com.yss.test.utils
+
+import java.io.FileInputStream
+import java.sql.{Connection, DriverManager}
+import java.util.Properties
+
+object MysqlUtil {
+
+  /**
+    * mysql数据库连接方法
+    * 通过配置文件连接数据库
+    */
+  def getConn(): Connection = {
+    val path = "mysqlConnectionProperties.properties"
+    val properties = new Properties()
+    properties.load(new FileInputStream(path))
+    val url = properties.getProperty("url")
+    val database = properties.getProperty("dataBase")
+    val userName = properties.getProperty("userName", "root")
+    val password = properties.getProperty("password")
+    val conn:Connection = DriverManager.getConnection(url+database, userName, password)
+    conn
+  }
+}
---
+++ src/test/scala/com/yss/test/utils/XMLReader.scala
@@ -0,0 +1,23 @@
+package com.yss.test.utils
+
+import org.apache.spark.sql.SparkSession
+
+object XMLReader {
+
+  /**
+    * 读取XML文件，解析成Row类型的RDD
+    * @param path xml文件路径
+    * @param sparkSession
+    * @return
+    */
+  def readXML(path:String, sparkSession: SparkSession) = {
+    sparkSession.read
+      .format("com.databricks.spark.xml")
+      .option("rowTag", "Security")
+      .load(path)
+      .rdd
+  }
+
+
+
+}
