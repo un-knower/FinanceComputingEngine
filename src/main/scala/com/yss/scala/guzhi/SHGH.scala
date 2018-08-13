28d0f5c60ebd199c207789df32da0fbe7e34e451
zhanghaisui <zhanghaisui@ysstech.com>
2018-08-13 16:50:01 +0800

    完善上海过户--张海绥

--- src/main/scala/com/yss/scala/guzhi/SHGH.scala
+++ src/main/scala/com/yss/scala/guzhi/SHGH.scala
@@ -3,25 +3,34 @@ package com.yss.scala.guzhi
 import com.yss.scala.dto.ShangHaiGuoHu
 import com.yss.scala.util.Util
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
-
 import scala.math.BigDecimal.RoundingMode
 
-
+/**
+  * @author : 张海绥
+  * @version : 2018-8-13
+  *  describe: 上海过户管理
+  *  目标表：SHGH
+  */
 object SHGH {
   def main(args: Array[String]): Unit = {
+
     val spark = SparkSession
       .builder()
       .appName("mytest")
       .master("local[2]")
       .getOrCreate()
+
     import com.yss.scala.dbf._
     import spark.implicits._
 
-    val dbfFileFrame: DataFrame = spark.sqlContext.dbfFile("C:\\Users\\YZM\\Desktop\\gh.dbf")
+    //读取上海过户目标文件，并将数据注册成一张临时表
+    val dbfFileFrame: DataFrame = spark.sqlContext.dbfFile("C:\\Users\\YZM\\Desktop\\gh23341.dbf")
     dbfFileFrame.createOrReplaceTempView("dbfFileTable")
 
+    //对上海过户文件进行sum汇总
     val sumDbfFileFrame: DataFrame = spark.sql("select GDDM,BCRQ,GSDM,ZQDM,BS,sum(CAST(CJSL AS decimal(15,2))) as CJSL,sum(CAST(CJJE AS decimal(15,2))) as CJJE from dbfFileTable group by GDDM,BCRQ,GSDM,ZQDM,BS")
 
+    //获取结果表中各个字段数据
     val dsResult: Dataset[ShangHaiGuoHu] = sumDbfFileFrame.map(line => {
 
       //上海过户默认费率
@@ -40,7 +49,6 @@ object SHGH {
       //风险金费率
       val rateFXJ = BigDecimal(0.00003)
 
-
       //买卖方向
       val BS = line.getAs[String]("BS")
       //日期
@@ -54,7 +62,7 @@ object SHGH {
       //交易席位号
       val FJYXWH = line.getAs[String]("GSDM")
       //买金额
-      var FBJE: BigDecimal = BigDecimal(0.0)
+      var FBJE: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
       if (BS == "B") FBJE = BigDecimal(String.valueOf(line.get(6))).setScale(2, RoundingMode.HALF_UP)
       //卖金额
       var FSJE: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
@@ -68,13 +76,11 @@ object SHGH {
       //买经手费
       var FBJSF: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
       if (BS == "B") FBJSF = (FBJE * rateJS).setScale(2, RoundingMode.HALF_UP)
-
       //卖经手费
       var FSJSF: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
       if (BS == "S") FSJSF = (FSJE * rateJS).setScale(2, RoundingMode.HALF_UP)
       //买印花税
       var FBYHS: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
-      if (BS == "B") FBYHS = (FBJE * rateYH).setScale(2, RoundingMode.HALF_UP)
       //卖印花税
       var FSYHS: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
       if (BS == "S") FSYHS = (FSJE * rateYH).setScale(2, RoundingMode.HALF_UP)
@@ -92,7 +98,7 @@ object SHGH {
       if (BS == "S") FSGHF = (FSJE * rateGH).setScale(2, RoundingMode.HALF_UP)
       //买佣金
       var FBYJ: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
-      if (BS == "B") FBYJ = (FBJE * rateYJ - FBZGF - FBGHF - FBYHS).setScale(2, RoundingMode.HALF_UP)
+      if (BS == "B") FBYJ = (FBJE * rateYJ - FBZGF - FBGHF-FBYHS ).setScale(2, RoundingMode.HALF_UP)
       //卖佣金
       var FSYJ: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
       if (BS == "S") FSYJ = (FSJE * rateYJ - FSZGF - FSGHF - FSYHS).setScale(2, RoundingMode.HALF_UP)
@@ -107,7 +113,7 @@ object SHGH {
       var FSFXJ: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
       if (BS == "S") FSFXJ = (FSJE * rateFXJ).setScale(2, RoundingMode.HALF_UP)
       //买实付金额
-      val FBSFJE: BigDecimal = (FBJE + FBJSF + FBZGF + FBGHF + FBYHS).setScale(2, RoundingMode.HALF_UP)
+      val FBSFJE: BigDecimal = (FBJE + FBJSF + FBZGF + FBGHF ).setScale(2, RoundingMode.HALF_UP)
       //卖实收金额
       val FSSSJE: BigDecimal = (FSJE - FSJSF - FSZGF - FSGHF - FSYHS).setScale(2, RoundingMode.HALF_UP)
       //证券标志，目前只处理GP
@@ -143,32 +149,33 @@ object SHGH {
       //回购收益,给一个默认值
       val FHGGAIN: BigDecimal = BigDecimal(0.0).setScale(2, RoundingMode.HALF_UP)
 
+      //返回一个对象，对象属性为表元数据信息
       ShangHaiGuoHu(
         FDATE,
         FINDATE,
         FZQDM,
         FSZSH,
         FJYXWH,
-        FBJE,
-        FSJE,
-        FBSL,
-        FSSL,
-        FBYJ,
-        FSYJ,
-        FBJSF,
-        FSJSF,
-        FBYHS,
-        FSYHS,
-        FBZGF,
-        FSZGF,
-        FBGHF,
-        FSGHF,
-        FBGZLX,
-        FSGZLX,
-        FBFXJ,
-        FSFXJ,
-        FBSFJE,
-        FSSSJE,
+        FBJE.formatted("%.2f"),
+        FSJE.formatted("%.2f"),
+        FBSL.formatted("%.2f"),
+        FSSL.formatted("%.2f"),
+        FBYJ.formatted("%.2f"),
+        FSYJ.formatted("%.2f"),
+        FBJSF.formatted("%.2f"),
+        FSJSF.formatted("%.2f"),
+        FBYHS.formatted("%.2f"),
+        FSYHS.formatted("%.2f"),
+        FBZGF.formatted("%.2f"),
+        FSZGF.formatted("%.2f"),
+        FBGHF.formatted("%.2f"),
+        FSGHF.formatted("%.2f"),
+        FBGZLX.formatted("%.2f"),
+        FSGZLX.formatted("%.2f"),
+        FBFXJ.formatted("%.2f"),
+        FSFXJ.formatted("%.2f"),
+        FBSFJE.formatted("%.2f"),
+        FSSSJE.formatted("%.2f"),
         FZQBZ,
         FYWBZ,
         FQSBZ,
@@ -181,16 +188,16 @@ object SHGH {
         FCHK,
         FZLH,
         FTZBZ,
-        FBQSGHF,
-        FSQSGHF,
+        FBQSGHF.formatted("%.2f"),
+        FSQSGHF.formatted("%.2f"),
         FGDDM,
-        FHGGAIN
+        FHGGAIN.formatted("%.2f")
       )
 
     })
+    //将结果输出到MySql中
     val dfResult = dsResult.toDF()
     Util.outputMySql(dfResult,"SHGH")
-
 
   }
 }
