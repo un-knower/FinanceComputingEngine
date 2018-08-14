3b6f9e3bae10fb952d38c902fe6ba51c1964ab0c
zhangkai <zhangkai_bj@ysstech.com>
2018-08-14 14:23:54 +0800

    更新代码

--- src/main/scala/com/yss/scala/guzhi/GuoHu_DBF.scala
+++ src/main/scala/com/yss/scala/guzhi/GuoHu_DBF.scala
@@ -3,55 +3,59 @@ package com.yss.scala.guzhi
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 
+import scala.math.BigDecimal.RoundingMode
+
 /**
   * @author 张锴
   * @version 2018-08-08
-  *  描述：上海过户表
-  *   源文件：gh.dbf
-  *   结果表：HZJKQS
+  *          描述：上海过户表
+  *          源文件：gh.dbf
+  *          结果表：HZJKQS
   */
 object GuoHu_DBF {
-  case class HZJKQS(FDATE: String,
-                    FINDATE: String,
-                    FZQDM: String,
-                    FSZSH: String,
-                    FJYXWH: String,
-                    FBJE: BigDecimal,
-                    FSJE: BigDecimal,
-                    FBSL: BigDecimal,
-                    FSSL: BigDecimal,
-                    FBYJ: BigDecimal,
-                    FSYJ: BigDecimal,
-                    FBJSF: BigDecimal,
-                    FSJSF: BigDecimal,
-                    FBYHS: BigDecimal,
-                    FSYHS: BigDecimal,
-                    FBZGF: BigDecimal,
-                    FSZGF: BigDecimal,
-                    FBGHF: BigDecimal,
-                    FSGHF: BigDecimal,
-                    FBGZLX: BigDecimal,
-                    FSGZLX: BigDecimal,
-                    FBFXJ: BigDecimal,
-                    FSFXJ: BigDecimal,
-                    FBSFJE: BigDecimal,
-                    FSSSJE: BigDecimal,
-                    FHGGAIN: BigDecimal,
-                    FZQBZ: String,
-                    FYWBZ: String,
-                    FQSBZ: String,
-                    FBQTF: String,
-                    FSQTF: String,
-                    ZQDM: String,
-                    FJYFS: String,
-                    FSH: String,
-                    FZZR: String,
-                    FCHK: String,
-                    FZLH: String,
-                    FTZBZ: String,
-                    FBQSGHF: BigDecimal,
-                    FSQSGHF: BigDecimal,
-                    FGDDM: String
+
+  case class HZJKQS(
+                     FDATE: String,
+                     FINDATE: String,
+                     FZQDM: String,
+                     FSZSH: String,
+                     FJYXWH: String,
+                     FBJE: String,
+                     FSJE: String,
+                     FBSL: String,
+                     FSSL: String,
+                     FBYJ: String,
+                     FSYJ: String,
+                     FBJSF: String,
+                     FSJSF: String,
+                     FBYHS: String,
+                     FSYHS: String,
+                     FBZGF: String,
+                     FSZGF: String,
+                     FBGHF: String,
+                     FSGHF: String,
+                     FBGZLX: String,
+                     FSGZLX: String,
+                     FBFXJ: String,
+                     FSFXJ: String,
+                     FBSFJE: String,
+                     FSSSJE: String,
+                     FHGGAIN: String,
+                     FZQBZ: String,
+                     FYWBZ: String,
+                     FQSBZ: String,
+                     FBQTF: String,
+                     FSQTF: String,
+                     ZQDM: String,
+                     FJYFS: String,
+                     FSH: String,
+                     FZZR: String,
+                     FCHK: String,
+                     FZLH: String,
+                     FTZBZ: String,
+                     FBQSGHF: String,
+                     FSQSGHF: String,
+                     FGDDM: String
                    )
 
   def main(args: Array[String]): Unit = {
@@ -61,30 +65,29 @@ object GuoHu_DBF {
       .master("local[2]")
       .getOrCreate()
     import com.yss.scala.dbf._
-    val value = spark.sqlContext.dbfFile("E:\\DBFFILE\\InterDoc\\gh.dbf").rdd.map(x => {
-      val Fdate = x.getAs[String]("BCRQ")
-      //日期
-      val FinDate = x.getAs[String]("BCRQ")
-      //读入日期
-      val FZqdm = x.getAs[String]("ZQDM")
-      //证券代码
-      val Fjyxwh = x.getAs[String]("GSDM")
-      //交易席位号
-      val ZQDM = x.getAs[String]("ZQDM")
-      //证券代码
-      val fgddm = x.getAs[String]("GDDM")
-      //股东代码
-      (Fdate + "_" + FinDate + "_" + FZqdm + "_" + Fjyxwh + "_" + ZQDM + "_" + fgddm, x)
-    }).groupByKey()
+    //hdfs://nscluster/yss/guzhi/gh.dbf
+    val value = spark.sqlContext.dbfFile("E:\\DBFFILE\\InterDoc\\gh23341.dbf").rdd.map(row => {
 
-    val v: RDD[HZJKQS] = value.map(t => {
+      val Fdate = row.getAs[String]("BCRQ") //日期
+
+      val FinDate = row.getAs[String]("BCRQ") //读入日期
+
+      val FZqdm = row.getAs[String]("ZQDM") //证券代码
+
+      val Fjyxwh = row.getAs[String]("GSDM") //交易席位号
+
+      val fgddm = row.getAs[String]("GDDM") //股东代码
+      val bs = row.getAs[String]("BS") //买卖
+
+      (Fdate + "_" + FinDate + "_" + FZqdm + "_" + Fjyxwh + "_" + fgddm + "_" + bs, row)
+    }).groupByKey().map(t => {
       val fs = t._1.split("_")
       val Fdate = fs(0)
       val FinDate = fs(1)
       val FZqdm = fs(2)
       val Fjyxwh = fs(3)
-      val ZQDM = fs(4)
-      val fgddm = fs(5)
+      val fgddm = fs(4)
+      val bs = fs(5)
 
       var Fbje = BigDecimal(0.0)
       var Fsje = BigDecimal(0.0)
@@ -100,14 +103,13 @@ object GuoHu_DBF {
       var FSzgf = BigDecimal(0.0)
       var FBghf = BigDecimal(0.0)
       var FSghf = BigDecimal(0.0)
-      val FBgzlx = BigDecimal(0)
-      val FSgzlx = BigDecimal(0)
+      val FBgzlx = "0"
+      val FSgzlx = "0"
       var FBfxj = BigDecimal(0.0)
       var FSfxj = BigDecimal(0.0)
-      var Fbsfje = BigDecimal(0.0)
-      var Fsssje = BigDecimal(0.0)
-      var FHggain = BigDecimal(0.0)
-      val FSzsh = "H"
+      var FHggain = "0"
+
+      val FSzsh = "H" //市场
       val FZqbz = "GP"
       val Fywbz = "PT"
       val FQsbz = "N"
@@ -119,57 +121,126 @@ object GuoHu_DBF {
       val FCHK = " "
       val fzlh = "0"
       val ftzbz = " "
-      val FBQsghf = BigDecimal(0.0)
-      val FsQsghf = BigDecimal(0.0)
-      //默认费率
-      val yongjin = 0.0025
-      val yinghua = 0.001
-      val guohu = 0.0005
-      val jingshou = 0.00011
-      val zhengguan = 0.00004
-      val fengxianjin = 0.00003
+      val FBQsghf = "0"
+      val FSQsghf = "0"
 
+      //默认费率
+      val rateYJ = BigDecimal(0.0025)
+      val rateJS = BigDecimal(0.00011)
+      val zk = BigDecimal(1)
+      val rateYH = BigDecimal(0.001)
+      val rateZG = BigDecimal(0.000041)
+      val rateGH = BigDecimal(0.00004)
+      val rateFXJ = BigDecimal(0.00003)
+
+      var sumCjje = BigDecimal(0)
+      var sumCjsl = BigDecimal(0)
+      var sumYj = BigDecimal(0)
+      var sumJsf = BigDecimal(0)
+      var sumYhs = BigDecimal(0)
+      var sumZgf = BigDecimal(0)
+      var sumGhf = BigDecimal(0)
+      var sumFxj = BigDecimal(0)
 
       val fileds = t._2.mkString(",").split(",")
-      val bs = fileds(13)
-      var cjje = BigDecimal(fileds(11))
-      var cjsl = BigDecimal(fileds(5))
-      for (x <- t._2) {
-        if ("B".equals(bs)) {
-          Fbje = cjje
-          FBsl = cjsl
-          Fbyj = Fbje * yongjin - zhengguan - guohu - yinghua
-          FBjsf = Fbje * jingshou
-          Fbyhs = Fbje * yinghua
-          FBzgf = Fbje * zhengguan
-          FBghf = Fbje * guohu
-          FBfxj = Fbje * fengxianjin
-          Fbsfje = Fbje + FBjsf + FBzgf + FBghf
-        } else {
-          Fsje = cjje
-          FSsl = cjsl
-          Fsyj = Fbje * yongjin - zhengguan - guohu - yinghua
-          FSjsf = Fbje * jingshou
-          Fsyhs = Fbje * yinghua
-          FSzgf = Fbje * zhengguan
-          FSghf = Fbje * guohu
-          FSfxj = Fbje * fengxianjin
-          Fsssje = Fsje - FSjsf - FSzgf - FSghf - Fsyhs
-        }
-      }
-      HZJKQS(Fdate, FinDate, FZqdm, FSzsh, Fjyxwh, Fbje, Fsje, FBsl, FSsl, Fbyj, Fsyj, FBjsf, FSjsf, Fbyhs, Fsyhs, FBzgf, FSzgf, FBghf
-        , FSghf, FBgzlx, FSgzlx, FBfxj, FSfxj, Fbsfje, Fsssje, FHggain, FZqbz, Fywbz, FQsbz, FBQTF, FSQTF, ZQDM, FJYFS, Fsh, FZZR, FCHK, fzlh, ftzbz
-        , FBQsghf, FsQsghf, fgddm)
-    })
+      //val Fjyxwh = fileds(4)
+      val ZQDM = fileds(7)
+      // val fgddm = fileds(0)
 
+      for (x <- t._2) {
+        val cjje = BigDecimal(x.getAs[String]("CJJE"))
+        //成交金额
+        val cjsl = BigDecimal(x.getAs[String]("CJSL"))
+        //成交数量
+        val jsf = cjje * rateJS * zk.setScale(2, RoundingMode.HALF_UP)
+        val yhs = cjje * rateYH * zk.setScale(2, RoundingMode.HALF_UP)
+        val zgf = cjje * rateZG * zk.setScale(2, RoundingMode.HALF_UP)
+        val ghf = cjje * rateGH * zk.setScale(2, RoundingMode.HALF_UP)
+        val fx = cjje * rateFXJ * zk.setScale(2, RoundingMode.HALF_UP)
+        val yj = cjje * rateYJ.setScale(2, RoundingMode.HALF_UP) - zgf - jsf
+        sumCjje = sumCjje + cjje
+        sumCjsl = sumCjsl + cjsl
+        sumYj = sumYj + yj
+        sumJsf = sumJsf + jsf
+        sumYhs = sumYhs + yhs
+        sumZgf = sumZgf + zgf
+        sumGhf = sumGhf + ghf
+        sumFxj = sumFxj + fx
+      }
+      if ("B".equals(bs)) {
+        Fbje = sumCjje
+        FBsl = sumCjsl
+        FBjsf = sumJsf
+        Fbyhs = BigDecimal(0)
+        FBzgf = sumZgf
+        FBghf = sumGhf
+        FBfxj = sumFxj
+        Fbyj = sumYj
+      } else {
+        Fsje = sumCjje
+        FSsl = sumCjsl
+        FSjsf = sumJsf
+        Fsyhs = sumYhs
+        FSzgf = sumZgf
+        FSghf = sumGhf
+        FSfxj = sumFxj
+        Fsyj = sumYj
+      }
+      val Fbsfje = (Fbje + FBjsf + FBzgf + FBghf).setScale(2, RoundingMode.HALF_UP)
+      val Fsssje = (Fsje - FSjsf - FSzgf - FSghf - Fsyhs).setScale(2, RoundingMode.HALF_UP)
+
+      HZJKQS(
+        Fdate,
+        FinDate,
+        FZqdm,
+        FSzsh,
+        Fjyxwh,
+        Fbje.formatted("%.2f"),
+        Fsje.formatted("%.2f"),
+        FBsl.formatted("%.2f"),
+        FSsl.formatted("%.2f"),
+        Fbyj.formatted("%.2f"),
+        Fsyj.formatted("%.2f"),
+        FBjsf.formatted("%.2f"),
+        FSjsf.formatted("%.2f"),
+        Fbyhs.formatted("%.2f"),
+        Fsyhs.formatted("%.2f"),
+        FBzgf.formatted("%.2f"),
+        FSzgf.formatted("%.2f"),
+        FBghf.formatted("%.2f"),
+        FSghf.formatted("%.2f"),
+        FBgzlx,
+        FSgzlx,
+        FBfxj.formatted("%.2f"),
+        FSfxj.formatted("%.2f"),
+        Fbsfje.formatted("%.2f"),
+        Fsssje.formatted("%.2f"),
+        FHggain,
+        FZqbz,
+        Fywbz,
+        FQsbz,
+        FBQTF,
+        FSQTF,
+        ZQDM,
+        FJYFS,
+        Fsh,
+        FZZR,
+        FCHK,
+        fzlh,
+        ftzbz,
+        FBQsghf,
+        FSQsghf,
+        fgddm
+      )
+    })
     import spark.implicits._
-    v.toDF()
+    value.toDF()
       .write.format("jdbc")
       .option("url", "jdbc:mysql://192.168.102.119:3306/JJCWGZ?useUnicode=true&characterEncoding=utf8")
       .option("dbtable", "HZJKQS")
-      .option("user", "root")
-      .option("password", "root1234")
-      .mode(SaveMode.Append)
+      .option("user", "test01")
+      .option("password", "test01")
+      .mode(SaveMode.Overwrite)
       .save()
   }
 }
