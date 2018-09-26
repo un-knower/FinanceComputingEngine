f6dfaf3c21e5cdec61e81d6332805fc98b9efc63
gonghailiang <gonghailiang@ysstech.com>
2018-09-26 15:43:01 +0800

    常量类

---
+++ gz-business/src/main/scala/com.yss.scala/guzhi/Constants.scala
@@ -0,0 +1,12 @@
+package com.yss.scala.guzhi
+
+/**
+  *  *@author  gonghailiang
+  *  *@version 2018-09-26 15:41
+  *  * describe: 
+  *  * 目标文件：
+  *  * 目标表：
+  *  */
+private object  Constants {
+
+}
