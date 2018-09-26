86addab75cc121681f880101e264950a695b90d1
MingZhang Wang <wangmingzhang@ysstech.com>
2018-09-26 14:16:11 +0800

    三方回购深交所业务代码及ETL
    Signed-off-by: MingZhang Wang <wangmingzhang@ysstech.com>

--- gz-business/src/main/scala/com.yss.scala/guzhi/ShenzhenStockExchangeTriPartyRepoETL.scala
+++ gz-business/src/main/scala/com.yss.scala/guzhi/ShenzhenStockExchangeTriPartyRepoETL.scala
@@ -1,14 +1,11 @@
 package com.yss.scala.guzhi
 
 import java.text.SimpleDateFormat
-import java.util.{Date, Properties}
+import java.util.Properties
 
-import com.yss.scala.util.Util
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
 import com.yss.scala.dbf.dbf._
-import org.apache.commons.lang.time.FastDateFormat
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import com.yss.scala.dto.{JGETL, MXETL}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 
 import scala.math.BigDecimal.RoundingMode
 
@@ -304,119 +301,119 @@ object ShenzhenStockExchangeTriPartyRepoETL {
 
 }
 
-case class MXETL(
-                  MXJSZH: String,
-                  MXBFZH: String,
-                  MXSJLX: String,
-                  MXYWLB: String,
-                  MXZQDM: String,
-                  MXJYDY: String,
-                  MXTGDY: String,
-                  MXZQZH: String,
-                  MXDDBH: String,
-                  MXYYB: String,
-                  MXZXBH: String,
-                  MXYWLSH: String,
-                  MXCJSL: String,
-                  MXQSSL: String,
-                  MXCJJG: String,
-                  MXQSJG: String,
-                  MXXYJY: String,
-                  MXPCBS: String,
-                  MXZQLB: String,
-                  MXZQZL: String,
-                  MXGFXZ: String,
-                  MXJSFS: String,
-                  MXHBDH: String,
-                  MXQSBJ: String,
-                  MXYHS: String,
-                  MXJYJSF: String,
-                  MXJGGF: String,
-                  MXGHF: String,
-                  MXJSF: String,
-                  MXSXF: String,
-                  MXQSYJ: String,
-                  MXQTFY: String,
-                  MXZJJE: String,
-                  MXSFJE: String,
-                  MXCJRQ: String,
-                  MXQSRQ: String,
-                  MXJSRQ: String,
-                  MXFSRQ: String,
-                  MXQTRQ: String,
-                  MXSCDM: String,
-                  MXJYFS: String,
-                  MXZQDM2: String,
-                  MXTGDY2: String,
-                  MXDDBH2: String,
-                  MXCWDH: String,
-                  MXPPHM: String,
-                  MXFJSM: String,
-                  MXBYBZ: String,
-                  FYWBZ: String,
-                  FJYBZ: String,
-                  FJETemp: String,    //"SFCS"时的成交金额
-                  FCSGHQXTemp: String,//"SFCS"时的购回期限
-                  FHggain: String,    //"SFCS"时的回购收益
-                  FRZLV: String       //"SFCS"时的融资利率
-                )
-
-case class JGETL(
-                  JGJSZH: String,
-                  JGBFZH: String,
-                  JGSJLX: String,
-                  JGYWLB: String,
-                  JGZQDM: String,
-                  JGJYDY: String,
-                  JGTGDY: String,
-                  JGZQZH: String,
-                  JGDDBH: String,
-                  JGYYB: String,
-                  JGZXBH: String,
-                  JGYWLSH: String,
-                  JGCJSL: String,
-                  JGQSSL: String,
-                  JGJSSL: String,
-                  JGCJJG: String,
-                  JGQSJG: String,
-                  JGXYJY: String,
-                  JGPCBS: String,
-                  JGZQLB: String,
-                  JGZQZL: String,
-                  JGGFXZ: String,
-                  JGLTLX: String,
-                  JGJSFS: String,
-                  JGHBDH: String,
-                  JGQSBJ: String,
-                  JGYHS: String,
-                  JGJYJSF: String,
-                  JGJGGF: String,
-                  JGGHF: String,
-                  JGJSF: String,
-                  JGSXF: String,
-                  JGQSYJ: String,
-                  JGQTFY: String,
-                  JGZJJE: String,
-                  JGSFJE: String,
-                  JGJSBZ: String,
-                  JGZYDH: String,
-                  JGCJRQ: String,
-                  JGQSRQ: String,
-                  JGJSRQ: String,
-                  JGFSRQ: String,
-                  JGQTRQ: String,
-                  JGSCDM: String,
-                  JGJYFS: String,
-                  JGZQDM2: String,
-                  JGTGDY2: String,
-                  JGDDBH2: String,
-                  JGFJSM: String,
-                  JGBYBZ: String,
-
-                  FYWBZ: String,
-                  FJYBZ: String,
-                  FJETemp: String,    //"SFCS"时的成交金额
-                  FCSGHQXTemp: String,//"SFCS"时的购回期限
-                  FHggain: String,    //"SFCS"时的回购收益
-                  FRZLV: String       //"SFCS"时的融资利率
-                )
+//case class MXETL(
+//                  MXJSZH: String,
+//                  MXBFZH: String,
+//                  MXSJLX: String,
+//                  MXYWLB: String,
+//                  MXZQDM: String,
+//                  MXJYDY: String,
+//                  MXTGDY: String,
+//                  MXZQZH: String,
+//                  MXDDBH: String,
+//                  MXYYB: String,
+//                  MXZXBH: String,
+//                  MXYWLSH: String,
+//                  MXCJSL: String,
+//                  MXQSSL: String,
+//                  MXCJJG: String,
+//                  MXQSJG: String,
+//                  MXXYJY: String,
+//                  MXPCBS: String,
+//                  MXZQLB: String,
+//                  MXZQZL: String,
+//                  MXGFXZ: String,
+//                  MXJSFS: String,
+//                  MXHBDH: String,
+//                  MXQSBJ: String,
+//                  MXYHS: String,
+//                  MXJYJSF: String,
+//                  MXJGGF: String,
+//                  MXGHF: String,
+//                  MXJSF: String,
+//                  MXSXF: String,
+//                  MXQSYJ: String,
+//                  MXQTFY: String,
+//                  MXZJJE: String,
+//                  MXSFJE: String,
+//                  MXCJRQ: String,
+//                  MXQSRQ: String,
+//                  MXJSRQ: String,
+//                  MXFSRQ: String,
+//                  MXQTRQ: String,
+//                  MXSCDM: String,
+//                  MXJYFS: String,
+//                  MXZQDM2: String,
+//                  MXTGDY2: String,
+//                  MXDDBH2: String,
+//                  MXCWDH: String,
+//                  MXPPHM: String,
+//                  MXFJSM: String,
+//                  MXBYBZ: String,
+//                  FYWBZ: String,
+//                  FJYBZ: String,
+//                  FJETemp: String,    //"SFCS"时的成交金额
+//                  FCSGHQXTemp: String,//"SFCS"时的购回期限
+//                  FHggain: String,    //"SFCS"时的回购收益
+//                  FRZLV: String       //"SFCS"时的融资利率
+//                )
+//
+//case class JGETL(
+//                  JGJSZH: String,
+//                  JGBFZH: String,
+//                  JGSJLX: String,
+//                  JGYWLB: String,
+//                  JGZQDM: String,
+//                  JGJYDY: String,
+//                  JGTGDY: String,
+//                  JGZQZH: String,
+//                  JGDDBH: String,
+//                  JGYYB: String,
+//                  JGZXBH: String,
+//                  JGYWLSH: String,
+//                  JGCJSL: String,
+//                  JGQSSL: String,
+//                  JGJSSL: String,
+//                  JGCJJG: String,
+//                  JGQSJG: String,
+//                  JGXYJY: String,
+//                  JGPCBS: String,
+//                  JGZQLB: String,
+//                  JGZQZL: String,
+//                  JGGFXZ: String,
+//                  JGLTLX: String,
+//                  JGJSFS: String,
+//                  JGHBDH: String,
+//                  JGQSBJ: String,
+//                  JGYHS: String,
+//                  JGJYJSF: String,
+//                  JGJGGF: String,
+//                  JGGHF: String,
+//                  JGJSF: String,
+//                  JGSXF: String,
+//                  JGQSYJ: String,
+//                  JGQTFY: String,
+//                  JGZJJE: String,
+//                  JGSFJE: String,
+//                  JGJSBZ: String,
+//                  JGZYDH: String,
+//                  JGCJRQ: String,
+//                  JGQSRQ: String,
+//                  JGJSRQ: String,
+//                  JGFSRQ: String,
+//                  JGQTRQ: String,
+//                  JGSCDM: String,
+//                  JGJYFS: String,
+//                  JGZQDM2: String,
+//                  JGTGDY2: String,
+//                  JGDDBH2: String,
+//                  JGFJSM: String,
+//                  JGBYBZ: String,
+//
+//                  FYWBZ: String,
+//                  FJYBZ: String,
+//                  FJETemp: String,    //"SFCS"时的成交金额
+//                  FCSGHQXTemp: String,//"SFCS"时的购回期限
+//                  FHggain: String,    //"SFCS"时的回购收益
+//                  FRZLV: String       //"SFCS"时的融资利率
+//                )
