fc3a5f82a94c4ab7d2759951b2498ae059929395
wangmingzhang <wangmingzhang@ysstech.com>
2018-10-14 17:42:32 +0800

    优化三方回购深交所代码

--- gz-business/src/main/scala/com.yss.scala/guzhi/ShenzhenStockExchangeTriPartyRepoETL.scala
+++ gz-business/src/main/scala/com.yss.scala/guzhi/ShenzhenStockExchangeTriPartyRepoETL.scala
@@ -5,7 +5,9 @@ import java.util.Properties
 
 import com.yss.scala.dbf.dbf._
 import com.yss.scala.dto.{JGETL, MXETL}
-import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import com.yss.scala.util.DateUtils
+import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
+import sun.management.snmp.AdaptorBootstrap.DefaultValues
 
 import scala.math.BigDecimal.RoundingMode
 
@@ -22,31 +24,27 @@ object ShenzhenStockExchangeTriPartyRepoETL {
     val sc = spark.sparkContext
 
     //原始数据
-    val sjsjg: DataFrame = spark.sqlContext.dbfFile("F:/work/数据文件/sjsjg")
-    val sjsmx: DataFrame = spark.sqlContext.dbfFile("F:/work/数据文件/sjsmx")
+    val sjsjg: DataFrame = spark.sqlContext.dbfFile("hdfs://192.168.102.120:8020/tmp/SFHGTest/SJSJG/sjsjg0723.dbf")
+    val sjsmx: DataFrame = spark.sqlContext.dbfFile("hdfs://192.168.102.120:8020/tmp/SFHGTest/sjsmx5.dbf")
 
-//    println(sjsjg.rdd.collect().toBuffer)
-
-    //    sjsjg.printSchema()
-    //    sjsmx.printSchema()
+    sjsmx.show()
 
     val sjsmxFilterRDD = sjsmx.rdd.filter(row => {
-      "20180709".equals(row.getAs[String]("MXCJRQ"))&&
-        "SFCS SFDQ SFXZ SFTG SFJZ".contains(row.getAs[String]("MXYWLB"))&&
-        "02".equals(row.getAs[String]("MXJYFS"))&&
+      "20180723".equals(row.getAs[String]("MXCJRQ")) &&
+        "SFCS SFDQ SFXZ SFTG SFJZ".contains(row.getAs[String]("MXYWLB")) &&
+        "02".equals(row.getAs[String]("MXJYFS")) &&
         "01".equals(row.getAs[String]("MXSJLX"))
     })
 
-    val sjsjgFilterRDD = sjsjg.rdd.filter(row =>{
-      "20180709".equals(row.getAs[String]("JGCJRQ"))&&
-        "SFCS SFDQ SFXZ SFTG SFJZ".contains(row.getAs[String]("JGYWLB"))&&
-        "02".equals(row.getAs[String]("JGJYFS"))&&
+    val sjsjgFilterRDD = sjsjg.rdd.filter(row => {
+      "20180723".equals(row.getAs[String]("JGCJRQ")) &&
+        "SFCS SFDQ SFXZ SFTG SFJZ".contains(row.getAs[String]("JGYWLB")) &&
+        "02".equals(row.getAs[String]("JGJYFS")) &&
         "01".equals(row.getAs[String]("JGSJLX"))
     })
 
-//    println(sjsmxFilterRDD.collect().toBuffer)
 
-    val MXvalueRDD = sjsmxFilterRDD.map(lines =>{
+    val MXvalueRDD = sjsmxFilterRDD.map(lines => {
 
       // 原数据
       val MXJSZH: String = lines.getAs[String]("MXJSZH")
@@ -73,8 +71,8 @@ object ShenzhenStockExchangeTriPartyRepoETL {
       val MXJSFS: String = lines.getAs[String]("MXJSFS")
       val MXHBDH: String = lines.getAs[String]("MXHBDH")
       val MXQSBJ: String = lines.getAs[String]("MXQSBJ")
-      val MXYHS: String = lines.getAs[String]("MXYHS")
-      val MXJYJSF: String = lines.getAs[String]("MXJYJSF")
+      var MXYHS: String = getRowFieldAsString(lines,"MXYHS","0.00")
+      var MXJYJSF: String = getRowFieldAsString(lines, "MXJYJSF", "0.00")
       val MXJGGF: String = lines.getAs[String]("MXJGGF")
       val MXGHF: String = lines.getAs[String]("MXGHF")
       val MXJSF: String = lines.getAs[String]("MXJSF")
@@ -98,36 +96,36 @@ object ShenzhenStockExchangeTriPartyRepoETL {
       val MXFJSM: String = lines.getAs[String]("MXFJSM")
       val MXBYBZ: String = lines.getAs[String]("MXBYBZ")
 
-        // 新加字段
-        val FYWBZ: String = new String // 业务标志
-        val FJYBZ: String = new String // 交易标志
-        val FJETemp: BigDecimal = BigDecimal(MXQSBJ).abs // "SFCS"时的成交金额
+      // 新加字段
+      val FYWBZ: String = new String // 业务标志
+      val FJYBZ: String = new String // 交易标志
+      val FJETemp: BigDecimal = BigDecimal(MXQSBJ).abs // "SFCS"时的成交金额
 
-        val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+      val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
 
-        val strYear1=MXQTRQ.substring(0,4)
-        val strMonth1 = MXQTRQ.substring(4,6)
-        val strDay1 = MXQTRQ.substring(6,8)
-        val MXQTRQDate = strYear1 +"-"+ strMonth1 + "-" + strDay1
+      val strYear1 = MXQTRQ.substring(0, 4)
+      val strMonth1 = MXQTRQ.substring(4, 6)
+      val strDay1 = MXQTRQ.substring(6, 8)
+      val MXQTRQDate = strYear1 + "-" + strMonth1 + "-" + strDay1
 
       println(MXQTRQDate)
 
-        val strYear2=MXCJRQ.substring(0,4)
-        val strMonth2 = MXCJRQ.substring(4,6)
-        val strDay2 = MXCJRQ.substring(6,8)
-        val MXCJRQDate = strYear2 +"-"+ strMonth2 + "-" + strDay2
+      val strYear2 = MXCJRQ.substring(0, 4)
+      val strMonth2 = MXCJRQ.substring(4, 6)
+      val strDay2 = MXCJRQ.substring(6, 8)
+      val MXCJRQDate = strYear2 + "-" + strMonth2 + "-" + strDay2
 
 
       val endDate = simpleDateFormat.parse(MXQTRQDate)
-        val startDate = simpleDateFormat.parse(MXCJRQDate)
+      val startDate = simpleDateFormat.parse(MXCJRQDate)
 
-        val FCSGHQXTemp: BigDecimal = BigDecimal((endDate.getTime - startDate.getTime)./(24*60*60*1000)).setScale(2,RoundingMode.HALF_UP) //"SFCS"时的购回期限 = 购回日期 - 首期日期
+      val FCSGHQXTemp: BigDecimal = BigDecimal((endDate.getTime - startDate.getTime)./(24 * 60 * 60 * 1000)) //"SFCS"时的购回期限 = 购回日期 - 首期日期
 
 
-        val FRZLV: BigDecimal = BigDecimal(MXCJJG)/100 // 回购利率
-        val FHggain: BigDecimal = FJETemp.*(FRZLV).*(FCSGHQXTemp)./(365).setScale(2, RoundingMode.HALF_UP) // 回购收益 = 回购金额×回购利率*购回期限/365；
+      val FRZLV: BigDecimal = BigDecimal(MXCJJG) // 回购利率
+      val FHggain: BigDecimal = FJETemp.*(FRZLV).*(FCSGHQXTemp)./(365).setScale(2, RoundingMode.HALF_UP) // 回购收益 = 回购金额×回购利率*购回期限/365；
 
-      MXETL(MXJSZH, MXBFZH, MXSJLX, MXYWLB, MXZQDM, MXJYDY, MXTGDY, MXZQZH, MXDDBH, MXYYB, MXZXBH, MXYWLSH, MXCJSL, MXQSSL, MXCJJG, MXQSJG, MXXYJY, MXPCBS, MXZQLB, MXZQZL, MXGFXZ, MXJSFS, MXHBDH, MXQSBJ, MXYHS, MXJYJSF, MXJGGF, MXGHF, MXJSF, MXSXF, MXQSYJ, MXQTFY, MXZJJE, MXSFJE, MXCJRQ, MXQSRQ, MXJSRQ, MXFSRQ, MXQTRQ, MXSCDM, MXJYFS, MXZQDM2, MXTGDY2, MXDDBH2, MXCWDH, MXPPHM, MXFJSM, MXBYBZ, FYWBZ, FJYBZ, FJETemp.setScale(2,RoundingMode.HALF_UP).toString(), FCSGHQXTemp.setScale(2,RoundingMode.HALF_UP).toString(), FHggain.setScale(2,RoundingMode.HALF_UP).toString(), FRZLV.setScale(4,RoundingMode.HALF_UP).toString())
+      MXETL(MXJSZH, MXBFZH, MXSJLX, MXYWLB, MXZQDM, MXJYDY, MXTGDY, MXZQZH, MXDDBH, MXYYB, MXZXBH, MXYWLSH, MXCJSL, MXQSSL, MXCJJG, MXQSJG, MXXYJY, MXPCBS, MXZQLB, MXZQZL, MXGFXZ, MXJSFS, MXHBDH, MXQSBJ, MXYHS, MXJYJSF, MXJGGF, MXGHF, MXJSF, MXSXF, MXQSYJ, MXQTFY, MXZJJE, MXSFJE, MXCJRQ, MXQSRQ, MXJSRQ, MXFSRQ, MXQTRQ, MXSCDM, MXJYFS, MXZQDM2, MXTGDY2, MXDDBH2, MXCWDH, MXPPHM, MXFJSM, MXBYBZ, FYWBZ, FJYBZ, FJETemp.setScale(2, RoundingMode.HALF_UP).toString(), FCSGHQXTemp.setScale(2, RoundingMode.HALF_UP).toString(), FHggain.setScale(2, RoundingMode.HALF_UP).toString(), FRZLV.setScale(4, RoundingMode.HALF_UP).toString())
 
     })
 
@@ -159,12 +157,12 @@ object ShenzhenStockExchangeTriPartyRepoETL {
       val JGJSFS: String = lines.getAs[String]("JGJSFS")
       val JGHBDH: String = lines.getAs[String]("JGHBDH")
       val JGQSBJ: String = lines.getAs[String]("JGQSBJ")
-      val JGYHS: String =lines. getAs[String]("JGYHS")
+      val JGYHS: String = lines.getAs[String]("JGYHS")
       val JGJYJSF: String = lines.getAs[String]("JGJYJSF")
       val JGJGGF: String = lines.getAs[String]("JGJGGF")
-      val JGGHF: String =lines. getAs[String]("JGGHF")
-      val JGJSF: String =lines. getAs[String]("JGJSF")
-      val JGSXF: String =lines. getAs[String]("JGSXF")
+      val JGGHF: String = lines.getAs[String]("JGGHF")
+      val JGJSF: String = lines.getAs[String]("JGJSF")
+      val JGSXF: String = lines.getAs[String]("JGSXF")
       val JGQSYJ: String = lines.getAs[String]("JGQSYJ")
       val JGQTFY: String = lines.getAs[String]("JGQTFY")
       val JGZJJE: String = lines.getAs[String]("JGZJJE")
@@ -184,30 +182,30 @@ object ShenzhenStockExchangeTriPartyRepoETL {
       val JGFJSM: String = lines.getAs[String]("JGFJSM")
       val JGBYBZ: String = lines.getAs[String]("JGBYBZ")
 
-        //新加字段
-        val FYWBZ: String = new String // 业务标志
-        val FJYBZ: String = new String // 交易标志
-        val FJETemp: BigDecimal = BigDecimal(JGQSBJ).abs // "SFCS"时的成交金额
+      //新加字段
+      val FYWBZ: String = new String // 业务标志
+      val FJYBZ: String = new String // 交易标志
+      val FJETemp: BigDecimal = BigDecimal(JGQSBJ).abs // "SFCS"时的成交金额
 
-        val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+      val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
 
-         val strYear1=JGQTRQ.substring(0,4)
-         val strMonth1 = JGQTRQ.substring(4,6)
-         val strDay1 = JGQTRQ.substring(6,8)
-         val JGQTRQDate = strYear1 +"-"+ strMonth1 + "-" + strDay1
+      val strYear1 = JGQTRQ.substring(0, 4)
+      val strMonth1 = JGQTRQ.substring(4, 6)
+      val strDay1 = JGQTRQ.substring(6, 8)
+      val JGQTRQDate = strYear1 + "-" + strMonth1 + "-" + strDay1
 
 
-         val strYear2=JGCJRQ.substring(0,4)
-         val strMonth2 = JGCJRQ.substring(4,6)
-         val strDay2 = JGCJRQ.substring(6,8)
-         val JGCJRQDate = strYear2 +"-"+ strMonth2 + "-" + strDay2
+      val strYear2 = JGCJRQ.substring(0, 4)
+      val strMonth2 = JGCJRQ.substring(4, 6)
+      val strDay2 = JGCJRQ.substring(6, 8)
+      val JGCJRQDate = strYear2 + "-" + strMonth2 + "-" + strDay2
 
-        val endDate = simpleDateFormat.parse(JGQTRQDate)
-        val startDate = simpleDateFormat.parse(JGCJRQDate)
+      val endDate = simpleDateFormat.parse(JGQTRQDate)
+      val startDate = simpleDateFormat.parse(JGCJRQDate)
 
-        val FCSGHQXTemp: BigDecimal = (endDate.getTime - startDate.getTime)/(24*60*60*1000) //"SFCS"时的购回期限 = 购回日期 - 首期日期
-        val FRZLV: BigDecimal = BigDecimal(JGCJJG) / 100 // 回购利率
-        val FHggain: BigDecimal = FJETemp.*(FRZLV).*(FCSGHQXTemp)./(365).setScale(2, RoundingMode.HALF_UP) // 回购收益 = 回购金额×回购利率*购回期限/365；
+      val FCSGHQXTemp: BigDecimal = (endDate.getTime - startDate.getTime) / (24 * 60 * 60 * 1000) //"SFCS"时的购回期限 = 购回日期 - 首期日期
+      val FRZLV: BigDecimal = BigDecimal(JGCJJG) / 100 // 回购利率
+      val FHggain: BigDecimal = FJETemp.*(FRZLV).*(FCSGHQXTemp)./(365).setScale(2, RoundingMode.HALF_UP) // 回购收益 = 回购金额×回购利率*购回期限/365；
 
 
       JGETL(JGJSZH,
@@ -271,149 +269,34 @@ object ShenzhenStockExchangeTriPartyRepoETL {
 
     import spark.implicits._
 
-    // 写入mysql
-
-//    Util.outputMySql(MXvalueRDD.toDF(), "sjsmxETL-wmz")
-//    Util.outputMySql(JGvalueRDD.toDF(), "sjsjgETL-wmz")
-
-//    def NowDate(): String = {
-//      val now: Date = new Date()
-//      val dateFormat: FastDateFormat = new FastDateFormat("yyyy-MM-dd")
-//      val date = dateFormat.format(now)
-//      return date
-//    }
-
     val properties = new Properties()
     properties.put("user", "root")
     properties.put("password", "root1234")
-    MXvalueRDD.toDF().write.mode(SaveMode.Overwrite).jdbc("jdbc:mysql://192.168.102.120:3306/JJCWGZ", "sjsmxETL_wmz", properties)
-    JGvalueRDD.toDF().write.mode(SaveMode.Overwrite).jdbc("jdbc:mysql://192.168.102.120:3306/JJCWGZ", "sjsjgETL_wmz", properties)
+    MXvalueRDD.toDF().write.mode(SaveMode.Append).jdbc("jdbc:mysql://192.168.102.120:3306/JJCWGZ", "sjsmxETL_wmz", properties)
+    JGvalueRDD.toDF().write.mode(SaveMode.Append).jdbc("jdbc:mysql://192.168.102.120:3306/JJCWGZ", "sjsjgETL_wmz", properties)
 
 
     spark.stop()
 
+
   }
+
   //写入mySql
 
 
   // 获取当前日期
+  private def getRowFieldAsString(row: Row, fieldName: String, defaultValues: String = "0.00"): String = {
+    var field = row.getAs[String](fieldName)
+    if (field == "") {
+      field = defaultValues
+    } else if (field == null){
+      field = ""
+    } else {
+      field = field.trim
+    }
+    field
+  }
 
 
 }
 
-//case class MXETL(
-//                  MXJSZH: String,
-//                  MXBFZH: String,
-//                  MXSJLX: String,
-//                  MXYWLB: String,
-//                  MXZQDM: String,
-//                  MXJYDY: String,
-//                  MXTGDY: String,
-//                  MXZQZH: String,
-//                  MXDDBH: String,
-//                  MXYYB: String,
-//                  MXZXBH: String,
-//                  MXYWLSH: String,
-//                  MXCJSL: String,
-//                  MXQSSL: String,
-//                  MXCJJG: String,
-//                  MXQSJG: String,
-//                  MXXYJY: String,
-//                  MXPCBS: String,
-//                  MXZQLB: String,
-//                  MXZQZL: String,
-//                  MXGFXZ: String,
-//                  MXJSFS: String,
-//                  MXHBDH: String,
-//                  MXQSBJ: String,
-//                  MXYHS: String,
-//                  MXJYJSF: String,
-//                  MXJGGF: String,
-//                  MXGHF: String,
-//                  MXJSF: String,
-//                  MXSXF: String,
-//                  MXQSYJ: String,
-//                  MXQTFY: String,
-//                  MXZJJE: String,
-//                  MXSFJE: String,
-//                  MXCJRQ: String,
-//                  MXQSRQ: String,
-//                  MXJSRQ: String,
-//                  MXFSRQ: String,
-//                  MXQTRQ: String,
-//                  MXSCDM: String,
-//                  MXJYFS: String,
-//                  MXZQDM2: String,
-//                  MXTGDY2: String,
-//                  MXDDBH2: String,
-//                  MXCWDH: String,
-//                  MXPPHM: String,
-//                  MXFJSM: String,
-//                  MXBYBZ: String,
-//                  FYWBZ: String,
-//                  FJYBZ: String,
-//                  FJETemp: String,    //"SFCS"时的成交金额
-//                  FCSGHQXTemp: String,//"SFCS"时的购回期限
-//                  FHggain: String,    //"SFCS"时的回购收益
-//                  FRZLV: String       //"SFCS"时的融资利率
-//                )
-//
-//case class JGETL(
-//                  JGJSZH: String,
-//                  JGBFZH: String,
-//                  JGSJLX: String,
-//                  JGYWLB: String,
-//                  JGZQDM: String,
-//                  JGJYDY: String,
-//                  JGTGDY: String,
-//                  JGZQZH: String,
-//                  JGDDBH: String,
-//                  JGYYB: String,
-//                  JGZXBH: String,
-//                  JGYWLSH: String,
-//                  JGCJSL: String,
-//                  JGQSSL: String,
-//                  JGJSSL: String,
-//                  JGCJJG: String,
-//                  JGQSJG: String,
-//                  JGXYJY: String,
-//                  JGPCBS: String,
-//                  JGZQLB: String,
-//                  JGZQZL: String,
-//                  JGGFXZ: String,
-//                  JGLTLX: String,
-//                  JGJSFS: String,
-//                  JGHBDH: String,
-//                  JGQSBJ: String,
-//                  JGYHS: String,
-//                  JGJYJSF: String,
-//                  JGJGGF: String,
-//                  JGGHF: String,
-//                  JGJSF: String,
-//                  JGSXF: String,
-//                  JGQSYJ: String,
-//                  JGQTFY: String,
-//                  JGZJJE: String,
-//                  JGSFJE: String,
-//                  JGJSBZ: String,
-//                  JGZYDH: String,
-//                  JGCJRQ: String,
-//                  JGQSRQ: String,
-//                  JGJSRQ: String,
-//                  JGFSRQ: String,
-//                  JGQTRQ: String,
-//                  JGSCDM: String,
-//                  JGJYFS: String,
-//                  JGZQDM2: String,
-//                  JGTGDY2: String,
-//                  JGDDBH2: String,
-//                  JGFJSM: String,
-//                  JGBYBZ: String,
-//
-//                  FYWBZ: String,
-//                  FJYBZ: String,
-//                  FJETemp: String,    //"SFCS"时的成交金额
-//                  FCSGHQXTemp: String,//"SFCS"时的购回期限
-//                  FHggain: String,    //"SFCS"时的回购收益
-//                  FRZLV: String       //"SFCS"时的融资利率
-//                )
