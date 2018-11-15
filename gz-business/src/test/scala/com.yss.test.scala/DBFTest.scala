adf1cdce26e3bd39c071191c2664e85d086bb5cb
wusong <wusong@ysstech.com>
2018-11-15 12:00:16 +0800

    修改Util为BasicUtils

--- gz-business/src/test/scala/com.yss.test.scala/DBFTest.scala
+++ gz-business/src/test/scala/com.yss.test.scala/DBFTest.scala
@@ -1,6 +1,6 @@
 package com.yss.test.scala
 
-import com.yss.scala.util.Util
+import com.yss.scala.util.{FceUtils}
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -12,11 +12,9 @@ object DBFTest {
     //注意引入隐式转换
     import com.yss.scala.dbf.dbf._
     val spark = SparkSession.builder().appName("mytest").master("local[*]").getOrCreate()
-    val df = spark.sqlContext.dbfFile("C:\\Users\\wuson\\Desktop\\GuZhi\\createdbf4.dbf")
-    df.rdd.saveAsTextFile("C:\\Users\\wuson\\Desktop\\GuZhi\\tt")
-
+    val date = FceUtils.getCsholiday(spark.sparkContext,"2018-02-16",0)
+    println(date)
     spark.stop()
-
     //    sess.sparkContext.hadoopFile("C:\\Users\\wuson\\Desktop\\new\\reff040704.txt", classOf[TextInputFormat], classOf[LongWritable], classOf[Text])
     //      .map(pair => new String(pair._2.getBytes, 0, pair._2.getLength, "GBK")).foreach(println(_))
     //      .saveAsTextFile("C:\\Users\\wuson\\Desktop\\new\\reff040705.txt")
